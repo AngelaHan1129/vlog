fa42f6b6d0d3a157ac604cb018e0dd6457401443
AngelaHan1129 <tinaowo111@gmail.com>
2026-08-06 22:30:13 +0800

    port change

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ app.include_router(vlog_router, prefix="/api")
 
 if __name__ == "__main__":
     # 使用 HTTP 啟動伺服器 (避開憑證問題)
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=2026, reload=True)
